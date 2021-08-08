d49ce93464a25e1ce1587ef1298b05ac36fc0ce5
Valasubramanian Sunmugavel <valasubramanian.sunmugavel@cgi.com>
2021-08-08 13:43:28 +0530

    docker for production env

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,49 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+              <Switch>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route path="/users">
+                  <Users />
+                </Route>
+                <Route path="/">
+                  <Home />
+                </Route>
+              </Switch>
+            </header>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+      <>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,9 +55,14 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </>
   );
 }
 
-export default App;
+function About() {
+  return <p><h2>About</h2></p>;
+}
+
+function Users() {
+  return <p><h2>Users</h2></p>;
+}
