45543e3ddcefda69e3e96811b7612e09da5316bf
Valasubramanian Sunmugavel <valasubramanian.sunmugavel@cgi.com>
2021-11-13 15:22:46 +0530

    Queue Processing function added

--- Azure/Azure.Function/ProcessFile/QueueProcessing/ProcessJsonDataToQueue.cs
+++ Azure/Azure.Function/ProcessFile/QueueProcessing/ProcessJsonDataToQueue.cs
@@ -11,13 +11,55 @@ namespace Azure.Functions
 {
     public static class ProcessJsonDataToQueue
     {
-        // Processing Step 1 : Read JSON data from blob and push to Storage Queue
+        // Processing Step 1 : Read JSON data from blob and push to Storage Queue as message
         [FunctionName("ProcessJsonDataToQueue")]
-        public static void Run([BlobTrigger("uploads/queue/{name}.json", Connection="AzureFileUploadStorage")] Stream jsonFile, 
+        public static void Run([BlobTrigger("uploads/queue-book-items/{name}.json", Connection="AzureFileUploadStorage")] Stream jsonFile, 
+                                [Queue("queue-book-items", Connection="AzureFileUploadStorage")] IAsyncCollector<string> booksQueue,
                                 string name, 
                                 ILogger log)
         {
+            log.LogInformation($"ProcessJsonDataToQueue FileName:{name}.json started");
             
+            BookModel[] data = ReadFileData(jsonFile, log);
+            
+            if(data != null && data.Length > 0)
+            {
+                bool isCompleted = PushFileDataToQueue(data, booksQueue, log);
+                if(isCompleted)
+                    log.LogInformation($"ProcessJsonDataToQueue FileName:{name}.json completed");
+            }
+        }
+
+        private static BookModel[] ReadFileData(Stream jsonFile, ILogger log)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(jsonFile);
+                string jsonContent = reader.ReadToEnd();
+                BookModel[] data = JsonConvert.DeserializeObject<BookModel[]>(jsonContent);
+                return data;
+            }
+            catch (Exception ex) {
+                log.LogError(ex.InnerException.Message);
+                return null;
+            }
+        }
+
+        private static bool PushFileDataToQueue(BookModel[] data, IAsyncCollector<string> booksQueue, ILogger log)
+        {
+            try 
+            {
+                foreach (var item in data)
+                {
+                    string jsonMessage = JsonConvert.SerializeObject(item);
+                    booksQueue.AddAsync(jsonMessage);
+                }
+                return true;
+            }
+            catch (Exception ex) {
+                log.LogError(ex.InnerException.Message);
+                return false;
+            }
         }
     }
 }
--- Azure/Azure.Function/ProcessFile/QueueProcessing/ProcessQueueMessageToNoSql.cs
+++ Azure/Azure.Function/ProcessFile/QueueProcessing/ProcessQueueMessageToNoSql.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Azure.Functions
 {
@@ -10,9 +11,26 @@ namespace Azure.Functions
         // Processing Step 2 : Process Storage Queue message to No SQL database
         [FunctionName("ProcessQueueMessageToNoSql")]
         public static void Run([QueueTrigger("queue-book-items", Connection="AzureFileUploadStorage")] string jsonMessage, 
+                                [CosmosDB("dbBooks", "bookContainer", ConnectionStringSetting="AzureCosmosDbConnection")] IAsyncCollector<BookModel> books,
                                 ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed");
+            log.LogInformation($"ProcessQueueMessageToNoSql started");
+            bool isCompleted = WriteBookData(books, jsonMessage, log);
+            if(isCompleted)
+                log.LogInformation($"ProcessQueueMessageToNoSql completed");
+        }
+
+        private static bool WriteBookData(IAsyncCollector<BookModel> books, string jsonMessage, ILogger log)
+        {
+            try {
+                BookModel book = JsonConvert.DeserializeObject<BookModel>(jsonMessage);
+                books.AddAsync(book);
+                return true;
+            }
+            catch (Exception ex) {
+                log.LogError(ex.InnerException.Message);
+                return false;
+            }
         }
     }
 }
