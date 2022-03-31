c29175002b109d9825cacb11bdd0746d69c9c7b0
Valasubramanian Sunmugavel <valasubramanian.sunmugavel@cgi.com>
2022-03-31 20:58:35 +0530

    XYZ Development

---
+++ XYZ.Identity/XYZ.Identity/Config.cs
@@ -0,0 +1,47 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace XYZ.Identity
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("scope1"),
+                new ApiScope("scope2"),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                // m2m client credentials flow client
+                new Client
+                {
+                    ClientId = "client-id-453434-m2m",
+                    ClientName = "Client Credentials Client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+                    AllowedScopes = { "scope1" }
+                },
+                // interactive client using code flow + pkce
+                new Client
+                {
+                    ClientId = "client-id-112317298-interactive",
+                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "https://localhost:5000/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:5000/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:5000/signout-callback-oidc" },
+                    AllowedScopes = { "openid", "profile", "scope2" }
+                }
+            };
+    }
+}
